c3ac71c639eb944c510767812067a06844d6c182
conixtract <conixtract@gmail.com>
2025-03-23 23:05:55 +0100

    fix: clean up install script and create config directory

--- install/install.sh
+++ install/install.sh
@@ -11,7 +11,7 @@ sudo apt update
 while IFS= read -r package; do
     # Skip empty lines, section headers (lines starting with '['), or comments (lines starting with '#')
     [[ -z "$package" || "$package" =~ ^[[:space:]]*\[ || "$package" =~ ^[[:space:]]*# ]] && continue
-    
+
     # Check if a script for the package exists in $SCRIPT_DIR
     if [[ -f "$SCRIPT_DIR/$package.sh" ]]; then
         echo "Running $package.sh for package '$package'..."
@@ -20,11 +20,12 @@ while IFS= read -r package; do
         echo "Installing '$package' using apt..."
         sudo apt install -y "$package"
     fi
-done < ./packages.ini
+done <./packages.ini
 
 ########################################################
 #                     symlinking
 ########################################################
+mkdir $PROJ_DIR/config -p
 ln -s $PROJ_DIR/config/sway ~/.config/sway
 ln -s $PROJ_DIR/config/alacritty ~/.config/alacritty
 ln -s $PROJ_DIR/config/rofi ~/.config/rofi
