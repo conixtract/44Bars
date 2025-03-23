6ac5b6ca9b505bea275c71cf5674780e9d44d8e5
conixtract <conixtract@gmail.com>
2025-03-23 23:25:43 +0100

    fix: correct project directory path in installation script

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJ_DIR="../$SCRIPT_DIR"
+PROJ_DIR="$SCRIPT_DIR/.."
 
 ########################################################
 #                installing packages
