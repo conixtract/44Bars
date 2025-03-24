3f7ac3abf89a39c4437e6d4e5a76dc40f622e5ea
conixtract <conixtract@gmail.com>
2025-03-24 12:55:31 +0100

    feat: add initial zsh configuration and installation script updates

--- install/install.sh
+++ install/install.sh
@@ -31,6 +31,7 @@ mkdir $HOME_DIR/.config -p
 ln -s $PROJ_DIR/config/sway $HOME_DIR/.config/sway
 ln -s $PROJ_DIR/config/alacritty $HOME_DIR/.config/alacritty
 ln -s $PROJ_DIR/config/rofi $HOME_DIR/.config/rofi
+ln -s $PROJ_DIR/config/zsh/.zshrc $HOME_DIR/.zshrc
 
 ########################################################
 #                     configuring lightdm
--- install/zsh.sh
+++ install/zsh.sh
@@ -1,7 +1,12 @@
 install() {
     sudo apt install -y zsh
     # set as default
-    chsh -s $(which zsh)
+    sudo chsh -s $(which zsh)
+
+    # oh my zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # install autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
 remove() {
