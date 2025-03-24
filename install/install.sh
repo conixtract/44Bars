eb2561118fd9f692bce4719fbd10aa07dcd567ab
conixtract <conixtract@gmail.com>
2025-03-24 13:48:03 +0100

    feat: update installation script to include swaylock configuration and symlink .zshrc

--- install/install.sh
+++ install/install.sh
@@ -27,12 +27,15 @@ done <./packages.ini
 ########################################################
 #                     symlinking
 ########################################################
+ln -s $PROJ_DIR/config/zsh/.zshrc $HOME_DIR/.zshrc
+
 mkdir $HOME_DIR/.config -p
 ln -s $PROJ_DIR/config/sway $HOME_DIR/.config/sway
-ln -s $PROJ_DIR/config/swaylock/config $HOME_DIR/.swaylock/config
 ln -s $PROJ_DIR/config/alacritty $HOME_DIR/.config/alacritty
 ln -s $PROJ_DIR/config/rofi $HOME_DIR/.config/rofi
-ln -s $PROJ_DIR/config/zsh/.zshrc $HOME_DIR/.zshrc
+
+mkdir $HOME_DIR/.swaylock -p
+ln -s $PROJ_DIR/config/swaylock/config $HOME_DIR/.swaylock/config
 
 ########################################################
 #                     configuring lightdm
