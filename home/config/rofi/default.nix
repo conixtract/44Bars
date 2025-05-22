5143529442175693e1c9651a542474a286bee9b5
conixtract <conixtract@gmail.com>
2025-05-22 15:21:02 +0200

    finally own custome dracula based rofi

--- home/config/rofi/default.nix
+++ home/config/rofi/default.nix
@@ -1,9 +1,21 @@
-{ config, pkgs, colors, lib, ... }:
+{
+  config,
+  pkgs,
+  colors,
+  lib,
+  ...
+}:
 
+let
+  DOTFILES = "/home/forestgump/44Bars/home/config";
+in
 {
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    configPath = "/tmp/rofi-generated-but-ignored.rasi"; # lul hack
   };
-  xdg.configFile."rofi/config.rasi".text = lib.readFile(./rofi-dracula.conf);
+  xdg.configFile."rofi/config.rasi".source = (
+    config.lib.file.mkOutOfStoreSymlink "${DOTFILES}/rofi/rofi.conf"
+  );
 }
